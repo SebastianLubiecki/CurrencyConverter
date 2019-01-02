5ae2a21716cdb1700bbd96d7f10cb1c79cb87389
Sebastian Lubiecki <sebastian.lubiecki@gmail.com>
2019-01-02 14:13:50 +0100

    Full implementation

---
+++ src/main/java/Comparator.java
@@ -0,0 +1,21 @@
+import ConectionWithAPI.REST_NBP;
+
+import java.io.IOException;
+
+class Comparator {
+
+    void compareToMonthAgo(REST_NBP rest_nbp, int value) throws IOException {
+
+        double[] arrayNow = REST_NBP.getAskBidRequest(rest_nbp.getCurrencyNow());
+        double[] arrayMouthAgo = REST_NBP.getAskBidRequest(rest_nbp.getCurrencyPast());
+        double changeTo = (value / arrayMouthAgo[1]);
+        double changeFrom = changeTo * arrayNow[0];
+        double result = value - changeFrom;
+        if (result > 0) {
+            System.out.println("If you changed " + rest_nbp.name() + " to PLN a month ago, you would earn " + result + " PLN");
+        } else {
+            System.out.println("If you changed " + rest_nbp.name() + " to PLN a month ago, you would lost " + result + " PLN");
+        }
+    }
+
+}
--- src/main/java/ConectionWithAPI/REST_NBP.java
+++ src/main/java/ConectionWithAPI/REST_NBP.java
@@ -2,22 +2,35 @@ package ConectionWithAPI;
 
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 public interface REST_NBP {
 
-    Currency getCurrency() throws IOException;
 
-     static JSONObject jsonObject(String url) throws IOException {
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = rd.readLine();
-            return new JSONObject(jsonText);
-        }
+    Currency getMidCurrency() throws IOException;
+
+    Currency getCurrencyNow() throws IOException;
+
+    Currency getCurrencyPast() throws IOException;
+
+    JSONObject jsonObject(String url) throws IOException;
+
+    String name() throws IOException;
+
+    static double countCurrencyByMidCourse(REST_NBP rest_nbp, int numberOfPLN) throws IOException {
+        String[] valuesArray = String.valueOf(rest_nbp.getMidCurrency().getRates()).split(",");
+        double value = Double.parseDouble(valuesArray[1].replace("mid=", ""));
+        double result = numberOfPLN / value;
+        return result;
     }
+
+    static double[] getAskBidRequest(Currency currency) throws IOException {
+        String[] valuesArray = String.valueOf(currency.getRates()).split(",");
+        double valueAsk = Double.parseDouble(valuesArray[1].replace("ask=", ""));
+        double valueBid = Double.parseDouble(valuesArray[2].replace("bid=", ""));
+        double[] resultArray = {valueAsk, valueBid};
+        return resultArray;
+    }
+
+
 }
