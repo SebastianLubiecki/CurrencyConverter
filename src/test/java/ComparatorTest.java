2afdfef8bc0ba94c64f4e9e79fb88bd2f32afeab
Sebastian Lubiecki <sebastian.lubiecki@gmail.com>
2019-01-06 12:38:15 +0100

    First test red

--- src/test/java/ComparatorTest.java
+++ src/test/java/ComparatorTest.java
@@ -22,25 +22,25 @@ public class ComparatorTest {
     //@InjectMocks
 
     @Before
-    public void setComparator() {
+    public void setComparator() throws IOException {
         comparator = new Comparator();
 
     }
 
     @Test
-    public void chfTest() throws IOException {
-//        ArrayList<Object> listNow = new ArrayList<>();
-//        listNow.add("asd, bid:4.00,ask:3.7675");
-//        ArrayList<Object> listPast = new ArrayList<>();
-//        listPast.add("asd, bid:2.00,ask:3.7675");
-//        double[] arrayNow = {4.00, 4.00};
-//        double[] arrayPast = {2.00, 2.00};
-//       // when(rest_chf.getCurrencyNow()).thenReturn(new Currency("a", "Frank", "chf", listNow));
-//        when(RestNBP.getAskBidRequest(rest_chf.getCurrencyNow())).thenReturn(arrayNow);
-//        //when(rest_chf.getCurrencyPast()).thenReturn(new Currency("a", "Frank", "chf", listPast));
-//        when(RestNBP.getAskBidRequest(rest_chf.getCurrencyNow())).thenReturn(arrayPast);
-//        double actual = comparator.compareToMonthAgo(rest_chf, 100);
-//        double expected = 50;
-//        assertTrue(String.valueOf(actual == expected), true);
+    public void simpleComparatorTest() throws IOException {
+        ArrayList<Object> listNow = new ArrayList<>();
+        listNow.add("asd, bid:4.00,ask:3.7675");
+        ArrayList<Object> listPast = new ArrayList<>();
+        listPast.add("asd, bid:2.00,ask:3.7675");
+        double[] arrayNow = {4.00, 4.00};
+        double[] arrayPast = {2.00, 2.00};
+        // when(rest_chf.getCurrencyNow()).thenReturn(new Currency("a", "Frank", "chf", listNow));
+        when(RestNBP.getAskBidRequest(rest_chf.getCurrencyNow())).thenReturn(arrayNow);
+        // when(rest_chf.getCurrencyPast()).thenReturn(new Currency("a", "Frank", "chf", listPast));
+        when(RestNBP.getAskBidRequest(rest_chf.getCurrencyNow())).thenReturn(arrayPast);
+        double actual = comparator.compareToMonthAgo(rest_chf, 100);
+        double expected = 50;
+        assertTrue(String.valueOf(actual == expected), true);
     }
 }
